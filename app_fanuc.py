854f83b66fe22e39cf3f3120795f70298f3b05bf
Gabriel Alfredo Regali <88169218+gabrielregali@users.noreply.github.com>
2026-04-13 14:13:25 -0300

    Actualizacion archivo app.py
    se migra a anthropic

--- app_fanuc.py
+++ app_fanuc.py
@@ -256,7 +256,7 @@ CONTEXTO:
             #answer = response.text
 
             response = anthropic_client.messages.create(
-                model="claude-3-5-sonnet-latest",
+                model="claude-3-haiku-20240307",
                 max_tokens=1000,
                 temperature=0,
                 messages=[
