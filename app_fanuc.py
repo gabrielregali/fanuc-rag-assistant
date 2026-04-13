d3ad00920d69a1a68ccfc4e7f0d8d4a5d7475c9e
Gabriel Alfredo Regali <88169218+gabrielregali@users.noreply.github.com>
2026-04-13 15:11:19 -0300

    Actualizacion archivo app.py
    uso de gemini 2.0

--- app_fanuc.py
+++ app_fanuc.py
@@ -239,7 +239,8 @@ CONTEXTO:
         try:
 
             response = gemini_client.models.generate_content(
-                model="gemini-2.5-flash",
+                #model="gemini-2.5-flash",
+                model="gemini-2.0-flash",
                 contents=f"{instruccion}\n\nPREGUNTA:\n{pregunta}"
             )
 
